12a78ea29f85a7aa4cc2a39f72f1b7693fc9b3a6
Matthew Zhu <75225488+Matthew244Z@users.noreply.github.com>
2024-03-05 18:46:40 -0500

    Add files via upload

--- MTE_380/Core/Src/main.c
+++ MTE_380/Core/Src/main.c
@@ -18,6 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include <stdbool.h>
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -43,12 +44,318 @@
 TIM_HandleTypeDef htim3;
 
 /* USER CODE BEGIN PV */
+//Creating a typedef for motor states
 typedef enum {
 	MOTOR_FORWARD = 0,
 	MOTOR_BACKWARD,
 	MOTOR_STOP
 }MotorStates_t;
 
+//Creating a typedef for Mission States
+typedef enum {
+	START_RECOGNITION = 0,
+	START_TO_TARGET,
+	ACQUIRE_TARGET,
+	TARGET_ACQUIRED,
+	TARGET_TO_START,
+	SAFE_ZONE,
+	TARGET_RELEASE,
+	MISSION_SUCCESSFUL
+}MissionStates_t;
+
+//Creating a typedef for Surfaces
+typedef enum {
+	RED = 0,
+	BLUE,
+	GREEN,
+	WOOD,
+	NOT_APPLICABLE
+}Surfaces_t;
+
+//State trackers
+bool AT_START_LINE = FALSE;
+bool AT_TARGET = FALSE;
+bool TARGET_PATH_REALIGNED = FALSE;
+
+//Defining Min and Max values for Colour Sensor
+int FCL_R_max = 57;
+int FCL_R_min = 6;
+int FCL_G_max = 55;
+int FCL_G_min = 6;
+int FCL_B_max = 39;
+int FCL_B_min = 4;
+
+int LCL_R_max = 51;
+int LCL_R_min = 5;
+int LCL_G_max =  53 ;
+int LCL_G_min = 3;
+int LCL_B_max =  37;
+int LCL_B_min = 3;
+
+int RCL_R_max = 41;
+int RCL_R_min = 4;
+int RCL_G_max = 41;
+int RCL_G_min = 3;
+int RCL_B_max = 30;
+int RCL_B_min = 3;
+
+//Defining all colour sensor parameters
+//FRONT COLOUR SENSOR PARAMETERS
+// FCL RED SURFACE PARAMETERS
+int FCL_RED_R_AVG = 215;
+int FCL_RED_R_M_10 = FCL_RED_R_AVG - 10;
+int FCL_RED_R_P_10 = FCL_RED_R_AVG +10;
+
+int FCL_RED_G_AVG = 71;
+int FCL_RED_G_M_10 = FCL_RED_R_AVG - 10;
+int FCL_RED_G_P_10 = FCL_RED_R_AVG +10;
+
+int FCL_RED_B_AVG = 70;
+int FCL_RED_B_M_10 = FCL_RED_R_AVG - 10;
+int FCL_RED_B_P_10 = FCL_RED_R_AVG +10;
+
+// FCL GREEN SURFACE PARAMETERS
+
+int FCL_GREEN_R_AVG = 100;
+int FCL_GREEN_R_M_10 = FCL_GREEN_R_AVG - 10;
+int FCL_GREEN_R_P_10 = FCL_GREEN_R_AVG +10;
+
+int FCL_GREEN_G_AVG = 108;
+int FCL_GREEN_G_M_10 = FCL_GREEN_R_AVG - 10;
+int FCL_GREEN_G_P_10 = FCL_GREEN_R_AVG +10;
+
+int FCL_GREEN_B_AVG = 76;
+int FCL_GREEN_B_M_10 = FCL_GREEN_R_AVG - 10;
+int FCL_GREEN_B_P_10 = FCL_GREEN_R_AVG +10;
+
+// FCL BLUE SURFACE PARAMETERS
+
+int FCL_BLUE_R_AVG = 185;
+int FCL_BLUE_R_M_10 = FCL_BLUE_R_AVG - 10;
+int FCL_BLUE_R_P_10 = FCL_BLUE_R_AVG +10;
+
+int FCL_BLUE_G_AVG = 197;
+int FCL_BLUE_G_M_10 = FCL_BLUE_R_AVG - 10;
+int FCL_BLUE_G_P_10 = FCL_BLUE_R_AVG +10;
+
+int FCL_BLUE_B_AVG = 212;
+int FCL_BLUE_B_M_10 = FCL_BLUE_R_AVG - 10;
+int FCL_BLUE_B_P_10 = FCL_BLUE_R_AVG +10;
+
+// FCL WOOD SURFACE PARAMETERS
+
+int FCL_WOOD_R_AVG = 230;
+int FCL_WOOD_R_M_10 = FCL_WOOD_R_AVG - 10;
+int FCL_WOOD_R_P_10 = FCL_WOOD_R_AVG +10;
+
+int FCL_WOOD_G_AVG = 203;
+int FCL_WOOD_G_M_10 = FCL_WOOD_R_AVG - 10;
+int FCL_WOOD_G_P_10 = FCL_WOOD_R_AVG +10;
+
+int FCL_WOOD_B_AVG = 176;
+int FCL_WOOD_B_M_10 = FCL_WOOD_R_AVG - 10;
+int FCL_WOOD_B_P_10 = FCL_WOOD_R_AVG +10;
+
+
+//LEFT COLOUR SENSOR PARAMETERS
+// LCL RED SURFACE PARAMETERS
+
+int LCL_RED_R_AVG = 215;
+int LCL_RED_R_M_10 = LCL_RED_R_AVG - 10;
+int LCL_RED_R_P_10 = LCL_RED_R_AVG +10;
+
+int LCL_RED_G_AVG = 71;
+int LCL_RED_G_M_10 = LCL_RED_R_AVG - 10;
+int LCL_RED_G_P_10 = LCL_RED_R_AVG +10;
+
+int LCL_RED_B_AVG = 70;
+int LCL_RED_B_M_10 = LCL_RED_R_AVG - 10;
+int LCL_RED_B_P_10 = LCL_RED_R_AVG +10;
+
+// LCL GREEN SURFACE PARAMETERS
+
+int LCL_GREEN_R_AVG = 100;
+int LCL_GREEN_R_M_10 = LCL_GREEN_R_AVG - 10;
+int LCL_GREEN_R_P_10 = LCL_GREEN_R_AVG +10;
+
+int LCL_GREEN_G_AVG = 108;
+int LCL_GREEN_G_M_10 = LCL_GREEN_R_AVG - 10;
+int LCL_GREEN_G_P_10 = LCL_GREEN_R_AVG +10;
+
+int LCL_GREEN_B_AVG = 76;
+int LCL_GREEN_B_M_10 = LCL_GREEN_R_AVG - 10;
+int LCL_GREEN_B_P_10 = LCL_GREEN_R_AVG +10;
+
+// LCL BLUE SURFACE PARAMETERS
+
+int LCL_BLUE_R_AVG = 185;
+int LCL_BLUE_R_M_10 = LCL_BLUE_R_AVG - 10;
+int LCL_BLUE_R_P_10 = LCL_BLUE_R_AVG +10;
+
+int LCL_BLUE_G_AVG = 197;
+int LCL_BLUE_G_M_10 = LCL_BLUE_R_AVG - 10;
+int LCL_BLUE_G_P_10 = LCL_BLUE_R_AVG +10;
+
+int LCL_BLUE_B_AVG = 212;
+int LCL_BLUE_B_M_10 = LCL_BLUE_R_AVG - 10;
+int LCL_BLUE_B_P_10 = LCL_BLUE_R_AVG +10;
+
+// LCL WOOD SURFACE PARAMETERS
+
+int LCL_WOOD_R_AVG = 230;
+int LCL_WOOD_R_M_10 = LCL_WOOD_R_AVG - 10;
+int LCL_WOOD_R_P_10 = LCL_WOOD_R_AVG +10;
+
+int LCL_WOOD_G_AVG = 203;
+int LCL_WOOD_G_M_10 = LCL_WOOD_R_AVG - 10;
+int LCL_WOOD_G_P_10 = LCL_WOOD_R_AVG +10;
+
+int LCL_WOOD_B_AVG = 176;
+int LCL_WOOD_B_M_10 = LCL_WOOD_R_AVG - 10;
+int LCL_WOOD_B_P_10 = LCL_WOOD_R_AVG +10;
+
+
+//RCL COLOUR SENSOR PARAMETERS
+// RCL RED SURFACE PARAMETERS
+
+int RCL_RED_R_AVG = 215;
+int RCL_RED_R_M_10 = RCL_RED_R_AVG - 10;
+int RCL_RED_R_P_10 = RCL_RED_R_AVG +10;
+
+int RCL_RED_G_AVG = 71;
+int RCL_RED_G_M_10 = RCL_RED_R_AVG - 10;
+int RCL_RED_G_P_10 = RCL_RED_R_AVG +10;
+
+int RCL_RED_B_AVG = 70;
+int RCL_RED_B_M_10 = RCL_RED_R_AVG - 10;
+int RCL_RED_B_P_10 = RCL_RED_R_AVG +10;
+
+// RCL GREEN SURFACE PARAMETERS
+
+int RCL_GREEN_R_AVG = 100;
+int RCL_GREEN_R_M_10 = RCL_GREEN_R_AVG - 10;
+int RCL_GREEN_R_P_10 = RCL_GREEN_R_AVG +10;
+
+int RCL_GREEN_G_AVG = 108;
+int RCL_GREEN_G_M_10 = RCL_GREEN_R_AVG - 10;
+int RCL_GREEN_G_P_10 = RCL_GREEN_R_AVG +10;
+
+int RCL_GREEN_B_AVG = 76;
+int RCL_GREEN_B_M_10 = RCL_GREEN_R_AVG - 10;
+int RCL_GREEN_B_P_10 = RCL_GREEN_R_AVG +10;
+
+// RCL BLUE SURFACE PARAMETERS
+
+int RCL_BLUE_R_AVG = 185;
+int RCL_BLUE_R_M_10 = RCL_BLUE_R_AVG - 10;
+int RCL_BLUE_R_P_10 = RCL_BLUE_R_AVG +10;
+
+int RCL_BLUE_G_AVG = 197;
+int RCL_BLUE_G_M_10 = RCL_BLUE_R_AVG - 10;
+int RCL_BLUE_G_P_10 = RCL_BLUE_R_AVG +10;
+
+int RCL_BLUE_B_AVG = 212;
+int RCL_BLUE_B_M_10 = RCL_BLUE_R_AVG - 10;
+int RCL_BLUE_B_P_10 = RCL_BLUE_R_AVG +10;
+
+// RCL WOOD SURFACE PARAMETERS
+
+int RCL_WOOD_R_AVG = 230;
+int RCL_WOOD_R_M_10 = RCL_WOOD_R_AVG - 10;
+int RCL_WOOD_R_P_10 = RCL_WOOD_R_AVG +10;
+
+int RCL_WOOD_G_AVG = 203;
+int RCL_WOOD_G_M_10 = RCL_WOOD_R_AVG - 10;
+int RCL_WOOD_G_P_10 = RCL_WOOD_R_AVG +10;
+
+int RCL_WOOD_B_AVG = 176;
+int RCL_WOOD_B_M_10 = RCL_WOOD_R_AVG - 10;
+int RCL_WOOD_B_P_10 = RCL_WOOD_R_AVG +10;
+
+
+//Identifying surface being seen by front colour sensor using getFCLcolour() function
+int FCL_R[10] = 0;
+int FCL_G[10] = 0;
+int FCL_B[10] = 0;
+
+int curr_R_sum = 0;
+int curr_G_sum = 0;
+int curr_B_sum = 0;
+
+int curr_R_average = 0;
+int curr_G_average = 0;
+int curr_B_average = 0;
+
+Surfaces_t FCL_Surface = NOT_APPLICABLE;
+
+Surfaces_t getFCLcolour(){
+
+	for (int i = 0; i<10; i++){
+		curr_R_sum += FCL_R[i];
+		curr_G_sum += FCL_G[i];
+		curr_B_sum += FCL_B[i];
+
+		if (i<9){
+		FCL_R[i] = FCL_R[i+1];
+		FCL_G[i] = FCL_G[i+1];
+		FCL_B[i] = FCL_B[i+1];
+		}
+/*
+		else{
+		FCL_R[i] = ReadRInput from colour sensor mapped to RGB Scale using max and min;
+		S2, S3 = RedFilter
+		Frequency_PW = Reading from sensor (SOUT)
+		Frequency_RGB = map(FCL_R_max, FCL_R_min, 0, 255)
+
+		FCL_G[i] = ReadGInput from colour sensor mapped to RGB Scale using max and min;
+		S2, S3 = GreenFilter
+		Frequency_PW = Reading from sensor (SOUT)
+		Frequency_RGB = map(FCL_G_max, FCL_G_min, 0, 255)
+
+		FCL_B[i] = ReadBInput from colour sensor mapped to RGB Scale using max and min;
+		S2, S3 = BlueFilter
+		Frequency_PW = Reading from sensor (SOUT)
+		Frequency_RGB = map(FCL_B_max, FCL_B_min, 0, 255)
+
+		}
+	}
+*/
+	curr_R_Average = curr_R_sum/10;
+	curr_G_Average = curr_G_sum/10;
+	curr_B_Average = curr_B_sum/10;
+
+	if (((FCL_RED_R_M_10 <= curr_R_average) && (curr_R_average <= FCL_RED_R_P_10))
+	&& ((FCL_RED_G_M_10 <= curr_G_average) && (curr_G_average <= FCL_RED_G_P_10))
+	&& ((FCL_RED_B_M_10 <= curr_B_average) && (curr_B_average <= FCL_RED_B_P_10)))
+		{
+		FCL_Surface = RED;
+		}
+
+	else if (((FCL_GREEN_R_M_10 <= curr_R_average) && (curr_R_average <= FCL_GREEN_R_P_10))
+		&& ((FCL_GREEN_G_M_10 <= curr_G_average) && (curr_G_average <= FCL_GREEN_G_P_10))
+		&& ((FCL_GREEN_B_M_10 <= curr_B_average) && (curr_B_average <= FCL_GREEN_B_P_10)))
+		{
+		FCL_Surface = GREEN;
+		}
+
+	else if (((FCL_BLUE_R_M_10 <= curr_R_average) && (curr_R_average <= FCL_BLUE_R_P_10))
+			&& ((FCL_BLUE_G_M_10 <= curr_G_average) && (curr_G_average <= FCL_BLUE_G_P_10))
+			&& ((FCL_BLUE_B_M_10 <= curr_B_average) && (curr_B_average <= FCL_BLUE_B_P_10)))
+		{
+		FCL_Surface = BLUE;
+		}
+
+	else if (((FCL_WOOD_R_M_10 <= curr_R_average) && (curr_R_average <= FCL_WOOD_R_P_10))
+				&& ((FCL_WOOD_G_M_10 <= curr_G_average) && (curr_G_average <= FCL_WOOD_G_P_10))
+				&& ((FCL_WOOD_B_M_10 <= curr_B_average) && (curr_B_average <= FCL_WOOD_B_P_10)))
+		{
+		FCL_Surface = WOOD;
+		}
+	else
+		FCL_Surface = NOT_APPLICABLE;
+
+	return FCL_Surface;
+}
 GPIO_PinState PB5_IN1 = GPIO_PIN_RESET;
 GPIO_PinState PB6_IN2 = GPIO_PIN_RESET;
 GPIO_PinState PB7_IN3 = GPIO_PIN_RESET;
@@ -60,6 +367,7 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_TIM3_Init(void);
 void motorMove(MotorStates_t dirLeft, int32_t chLeft, MotorStates_t dirRight, int32_t chRight);
+
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -92,7 +400,53 @@ int main(void)
 	// Timer 3 has a period size of 16 bits, 0 - 65535, ARR set to 65534 (max period)
 	// TIM3->CCRX = PWM_Durity_Cycle = CCR/ARR
 
+Surfaces_t MISIION_STATE;
 
+MISSION_STATE = START_RECOGNITION;
+SetLED(START_RECOGNITION);
+
+While(At_start_line != 1){
+	- Surface_FCL = getFCLcolour();
+	- Surface_RCL = getRCLcolour();
+	- Surface_LCL = getLCLcolour();
+	- While (FCL != RED && LCL != RED && RCL != RED)
+		○ Move Forward;
+		○ FCL = getFCLcolour();
+		○ RCL = getRCLcolour();
+		○ LCL = getLCLcolour();
+	- Set At Start Line = 1;
+}
+
+State = Start_to_Target
+SetLED(Start_to_Target);
+
+While(At_Target != 1){
+	- FCL = getFCLcolour();
+	- RCL = getRCLcolour();
+	- LCL = getLCLcolour();
+	- If (FCL == RED, RCL == WOOD, LCL == WOOD)
+		○ MoveForward
+	- Else
+		○ Stop
+		○ Turn_or_Realign(Forward)
+	- If (FCL == BLUE)
+		○ At_Target = 1;
+}
+
+Stop for 3 seconds
+
+State = Target_to_Start
+SetLED(Target_to_Start);
+
+While(At_Start_Line != 1){
+	- FCL = getFCLcolour();
+	- RCL = getRCLcolour();
+	- LCL = getLCLcolour();
+	- If (FCL == RED, RCL == WOOD, LCL == WOOD)
+		○ MoveReverse
+	- Else
+		○ Stop
+Turn_or_Realign(Reverse)
 	while (1)
 	{
 		motorMove(MOTOR_FORWARD, 65535, MOTOR_FORWARD, 65535);
